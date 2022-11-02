3717a781c19a4f63bfb6536f920b514e58e28558
55soup <w2118@e-mirim.hs.kr>
2022-11-02 11:07:16 +0900

    js 이전, 다음 달 변경해도 이벤트 처리하자, 작년 정규식 수정

--- 20220907_grid/js/dynamic.js
+++ 20220907_grid/js/dynamic.js
@@ -8,6 +8,25 @@ let year = now.getFullYear();
 let month = now.getMonth(); //0~11
 month++;
 
+//AJAX 급식데이터 가져오자
+// .date-grid-container > .grid-item에 mouseover 이벤트 발생하면, handler를 지정하자
+// url 호출하자
+const handler = (event)=>{
+    let date = event.target.innerHTML; //date str형식으로 가져옴.
+    const KEY = "a7cc721e31ef4e5199636b84dd243813";
+    const ATPT_OFCDC_SC_CODE = "B10"; //서울특별시교육청
+    const SD_SCHUL_CODE = "7010569"; //미림여자정보과학고등학교
+    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.padStart(2, "0")}`; //2022년10월19일
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+    url += `?KEY=${KEY}`
+    url += `&TYPE=json`
+    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`
+    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`
+    url += `&MLSV_YMD=${MLSV_YMD}`
+    console.log(url)
+    getMenuByAPI(url)
+}
+
 const setCalendar = (year, month) => {
     // 1일이 무슨 요일?
     let firstDate = new Date(year, month-1, 1);
@@ -45,6 +64,13 @@ const setCalendar = (year, month) => {
     // 1일에 해당하는 div를 grid-column-start: 요일 + 1;
     let firstDateDiv = dateGridContainerDiv.getElementsByClassName("grid-item")[0];
     firstDateDiv.style.gridColumnStart = firstDay+1;
+
+    //mouseover event 처리
+    let gridItems = dateGridContainerDiv.getElementsByClassName("grid-item");
+    for(let gridItem of gridItems){
+        // console.log(gridItem);
+        gridItem.onmouseover = handler;
+    }
 }
 
 setCalendar(year, month);
@@ -85,24 +111,7 @@ const initButton  = () => {
 
 initButton()
 
-//AJAX 급식데이터 가져오자
-// .date-grid-container > .grid-item에 mouseover 이벤트 발생하면, handler를 지정하자
-// url 호출하자
-const handler = (event)=>{
-    let date = event.target.innerHTML; //date str형식으로 가져옴.
-    const KEY = "a7cc721e31ef4e5199636b84dd243813";
-    const ATPT_OFCDC_SC_CODE = "B10"; //서울특별시교육청
-    const SD_SCHUL_CODE = "7010569"; //미림여자정보과학고등학교
-    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.padStart(2, "0")}`; //2022년10월19일
-    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
-    url += `?KEY=${KEY}`
-    url += `&TYPE=json`
-    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`
-    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`
-    url += `&MLSV_YMD=${MLSV_YMD}`
-    console.log(url)
-    getMenuByAPI(url)
-}
+
 // AJAX로 url 호출하자(Asynchronous JavaScript And XML)
 let getMenuByAPI = (url)=>{
     // XMLHttpRequest 만들자
@@ -140,16 +149,19 @@ const showMenu = (jsonString) =>{
         // (5.13.) 삭제
         // (열고, 숫자, 여러개 ) 닫고
         breakfastMenu = breakfastMenu.replace(/\([0123456789\.]+\)/g,"")
+        breakfastMenu = breakfastMenu.replace(/[0123456789\.]+/g,"")
     } catch {
     }
     try{
         lunchMenu = json["mealServiceDietInfo"][1]["row"][1]["DDISH_NM"]
         lunchMenu = lunchMenu.replace(/\([0-9\.]+\)/g,"")
+        lunchMenu = lunchMenu.replace(/[0123456789\.]+/g,"")
     } catch {
     }
     try{
         dinnerMenu = json["mealServiceDietInfo"][1]["row"][2]["DDISH_NM"]
         dinnerMenu = dinnerMenu.replace(/\([0-9\.]+\)/g,"")
+        dinnerMenu = dinnerMenu.replace(/[0123456789\.]+/g,"")
     } catch {
     }
     // 조식, 중식, 석식 -> HTML
